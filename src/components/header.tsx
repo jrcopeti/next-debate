b58a4a20f949fcc3f525fa0aef4bfd6d37a9f53b
Jose Copeti <jrcopeti@gmail.com>
2024-02-23 18:18:51 +0100

    header implemented

---
+++ src/components/header.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Input,
+  Button,
+  Avatar,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import { auth } from "@/auth";
+import * as actions from "@/actions";
+
+async function Header() {
+  const session = await auth();
+
+  let authContent: React.ReactNode;
+  if (session?.user) {
+    authContent = (
+      <Popover>
+        <PopoverTrigger>
+          <Avatar src={session.user.image || ""} />
+        </PopoverTrigger>
+        <PopoverContent>
+          <div className="p-4">
+            <form action={actions.signOut}>
+              <Button type="submit">Sign Out</Button>
+            </form>
+          </div>
+        </PopoverContent>
+      </Popover>
+    );
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="secondary" variant="bordered">
+              Sign in
+            </Button>
+          </form>
+        </NavbarItem>
+        <NavbarItem>
+          <form action={actions.signIn}>
+            <Button type="submit" color="primary" variant="flat">
+              Sign Up
+            </Button>
+          </form>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Debate
+        </Link>
+      </NavbarBrand>
+
+      <NavbarContent>
+        <Input />
+      </NavbarContent>
+
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
+}
+
+export default Header;
