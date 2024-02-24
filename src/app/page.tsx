fe7eeec83a81c4fedbebad97887894b3f58ba2f5
Jose Copeti <jrcopeti@gmail.com>
2024-02-24 12:28:15 +0100

    topic create form popover

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@nextui-org/react";
-import * as actions from "@/actions";
-import { auth } from "@/auth";
-import Profile from "@/components/profile";
 
-export default async function Home() {
+import TopicCreateForm from "@/components/topics/topic-create-form";
+
+export default function Home() {
   return (
-    <div>Home Page</div>
-
-  )
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="text-xl m-2">Top Posts</h1>
+      </div>
+      <div>
+        <TopicCreateForm />
+      </div>
+    </div>
+  );
 }
--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,5 @@
+
+
 function TopicShowPage() {
   return (
     <div>
